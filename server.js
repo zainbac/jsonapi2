6214d6930030a9b45615a3630f1d96d3f981374a
unknown <zainbac@gmail.com>
2020-09-12 14:22:24 +0800

    updateapi2

--- server.js
+++ server.js
@@ -7,59 +7,63 @@ app.get('/', function (req, res) {
 app.get('/api/courses', (req, res) => {
   res.send([
     {
-      id: '0',
-      title: 'ZBC PHARMACY',
-      barcode: '12345',
-      picture: require('./../../../../assets/COUPON0.png'),
-      content: (
-        <Text style={{ textAlign: 'center' }}>
-          {' '}
-          HANDBRAKE SPAREPART PRICE : RM24
-        </Text>
-      ),
+    id: '0',
+    title: 'ZBC PHARMACY',
+    barcode: '12345',
+    picture: {
+      uri: 'https://i.ibb.co/Stpswg9/COUPON0.png',
     },
-    {
-      id: '1',
-      title: 'ZBC PHARMACY',
-      barcode: '12315',
-      picture: require('./../../../../assets/COUPON1.png'),
-      content: <Text>WHEELCHAIR TYRE 22,24 INCH : RM99</Text>,
+
+    content: ' HANDBRAKE SPAREPART PRICE : RM24',
+  },
+  {
+    id: '1',
+    title: 'ZBC PHARMACY',
+    barcode: '12315',
+    picture: { uri: 'https://i.ibb.co/TBN0BSC/COUPON1.png' },
+    content: 'WHEELCHAIR TYRE 22,24 INCH : RM99',
+  },
+  {
+    id: '2',
+    barcode: '123152',
+    title: 'ZBC PHARMACY',
+    picture: {
+      uri: 'https://i.ibb.co/hy549WW/COUPON2.png',
     },
-    {
-      id: '2',
-      barcode: '123152',
-      title: 'ZBC PHARMACY',
-      picture: require('./../../../../assets/COUPON2.png'),
-      content: <Text>CASTOR WHEELCHAIR 8IN NARROW : RM33</Text>,
-    },
-    {
-      id: '3',
-      barcode: '123152',
-      title: 'ZBC PHARMACY ',
-      picture: require('./../../../../assets/COUPON3.png'),
-      content: <Text> MAG WHEEL & FF TYRE 16IN : RM130</Text>,
-    },
-    {
-      id: '4',
-      barcode: '123152',
-      title: 'ZBC PHARMACY ',
-      picture: require('./../../../../assets/COUPON4.png'),
-      content: <Text>S'PART-ARMREST PAD 10IN RUBBER : RM31</Text>,
-    },
-    {
-      id: '5',
-      barcode: '123152',
-      title: 'ZBC PHARMACY ',
-      picture: require('./../../../../assets/COUPON5.png'),
-      content: <Text>S'PART-6/8IN RUBBER TIP (2PCS) : RM8.60</Text>,
-    },
-    {
-      id: '6',
-      barcode: '123152',
-      title: 'ZBC PHARMACY ',
-      picture: require('./../../../../assets/COUPON6.png'),
-      content: <Text>FOOTPLATE PLASTIC FOR WHEELCHAIR (1PAIR) : RM66</Text>,
-    },
+    content: 'CASTOR WHEELCHAIR 8IN NARROW : RM33',
+  },
+
+  {
+    id: '3',
+    barcode: '123152',
+    title: 'ZBC PHARMACY ',
+    picture: { uri: 'https://i.ibb.co/QrXSrbd/COUPON3.png' },
+    content: 'MAG WHEEL & FF TYRE 16IN : RM130',
+  },
+
+  {
+    id: '4',
+    barcode: '123152',
+    title: 'ZBC PHARMACY ',
+    picture: { uri: 'https://i.ibb.co/LvF4BJf/COUPON4.png' },
+    content: 'PART-ARMREST PAD 10IN RUBBER : RM31',
+  },
+
+  {
+    id: '5',
+    barcode: '123152',
+    title: 'ZBC PHARMACY ',
+    picture: { uri: 'https://i.ibb.co/dGm7Mvq/COUPON5.png' },
+    content: 'PART-6/8IN RUBBER TIP (2PCS) : RM8.60',
+  },
+
+  {
+    id: '6',
+    barcode: '123152',
+    title: 'ZBC PHARMACY ',
+    picture: { uri: 'https://i.ibb.co/C91Q8hb/COUPON6.png' },
+    content: 'FOOTPLATE PLASTIC FOR WHEELCHAIR (1PAIR) : RM66',
+  },
   ]);
 });
 app.get('/api/posts/:year/:month', (req, res) => {
